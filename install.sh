4ab29a6b68cdb4f6706063a4bee2dca5d46a4c77
Tim Rabbetts <timrabbetts@gmail.com>
2020-08-03 11:57:56 +0100

    tweak

--- install.sh
+++ install.sh
@@ -16,6 +16,8 @@ sudo apt-add-repository ppa:ansible/ansible
 
 sudo apt-get update
 
+sudo apt install ansible -y
+
 # Install node
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install nodejs -y
